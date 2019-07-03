384616273c33dc348c2a608018c9a272e26be20d
Kushal Kulasuriya <kushalkulasuriya96@gmail.com>
2019-07-03 18:43:09 +1000

    Updates

---
+++ precache-manifest.e2c09709c4cbbd96f1c8e500e8b2caa4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7def94ef16179aafc2ab2b242fefabfe",
+    "url": "/react-tute/index.html"
+  },
+  {
+    "revision": "502f0c9b80c80d810c91",
+    "url": "/react-tute/static/css/main.f612f6a5.chunk.css"
+  },
+  {
+    "revision": "3a9e33913cab0720716d",
+    "url": "/react-tute/static/js/2.86a2265c.chunk.js"
+  },
+  {
+    "revision": "502f0c9b80c80d810c91",
+    "url": "/react-tute/static/js/main.847d12fa.chunk.js"
+  },
+  {
+    "revision": "7bde421fd0f4adcf742e",
+    "url": "/react-tute/static/js/runtime~main.81e717b5.js"
+  }
+]);
